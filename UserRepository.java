19d28e653017df740a0bb0f1d656c0ce7b688b90
Rohit Gade <155424199+Rohitgade14@users.noreply.github.com>
2024-10-02 16:47:59 +0530

    Add files via upload

---
+++ UserRepository.java
@@ -0,0 +1,9 @@
+package com.sbi.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sbi.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
